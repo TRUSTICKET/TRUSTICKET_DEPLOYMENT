07e80c208206c002b71843db33fdca9a20ded167
Jaehwi-So <sjh9708@gmail.com>
2025-05-21 11:23:58 +0900

    chore : Ingress 라우팅 규칙에서 Rewrite 제거

---
+++ services/deploy.sh
@@ -0,0 +1,33 @@
+
+kubectl apply -f ./configmap.yaml
+kubectl apply -f ./secret.yaml
+
+kubectl apply -f ./booking-service/tt-booking-service-deployment.yaml
+kubectl apply -f ./booking-service/tt-booking-service-service.yaml
+
+kubectl apply -f ./content-service/tt-content-service-deployment.yaml
+kubectl apply -f ./content-service/tt-content-service-service.yaml
+
+kubectl apply -f ./core-service/tt-core-service-deployment.yaml
+kubectl apply -f ./core-service/tt-core-service-service.yaml
+
+kubectl apply -f ./payment-service/tt-payment-service-deployment.yaml
+kubectl apply -f ./payment-service/tt-payment-service-service.yaml
+
+kubectl apply -f ./resources-service/tt-resources-service-pvc.yaml
+kubectl apply -f ./resources-service/tt-resources-service-deployment.yaml
+kubectl apply -f ./resources-service/tt-resources-service-service.yaml
+
+kubectl apply -f ./booking-service/tt-booking-service-hpa.yaml
+kubectl apply -f ./content-service/tt-content-service-hpa.yaml
+kubectl apply -f ./core-service/tt-core-service-hpa.yaml
+kubectl apply -f ./payment-service/tt-payment-service-hpa.yaml
+kubectl apply -f ./resources-service/tt-resources-service-hpa.yaml
+
+kubectl apply -f ./ingress.yaml
+
+kubectl rollout restart deployment tt-core-service
+kubectl rollout restart deployment tt-booking-service
+kubectl rollout restart deployment tt-payment-service
+kubectl rollout restart deployment tt-content-service
+kubectl rollout restart deployment tt-resources-service
